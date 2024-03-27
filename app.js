3e2dfce6787d9371592022ee7c0b8fb4833f04c4
Angel <angel@Angels-MacBook-Air.local>
2024-03-27 15:32:17 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+module.exports = app;
